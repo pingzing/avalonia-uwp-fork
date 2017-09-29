32cd608907f6d259b0333f527f060ba649ff49b1
Neil McAlister <neil.mcalister@futurice.com>
2017-09-29 17:03:32 +0300

    Maybe skeleton of TopLevelImpl?

--- Avalonia.UWP/PlatformThreadingInterfaceImpl.cs
+++ Avalonia.UWP/PlatformThreadingInterfaceImpl.cs
@@ -1,28 +1,57 @@
 ﻿using System;
 using System.Threading;
 using Avalonia.Platform;
+using Windows.UI.Core;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using System.Reactive.Disposables;
 
 namespace Avalonia.UWP
 {
     internal class PlatformThreadingInterfaceImpl : IPlatformThreadingInterface
     {
-        public bool CurrentThreadIsLoopThread => throw new NotImplementedException();
+        private bool _signaled;
+
+        public bool CurrentThreadIsLoopThread => CoreWindow.GetForCurrentThread().Dispatcher.HasThreadAccess;
 
         public event Action Signaled;
 
         public void RunLoop(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            // We use the CoreDispatcher's event loop instead of our own RunLoop.
+            return;
         }
 
         public void Signal()
         {
-            throw new NotImplementedException();
+            lock (this)
+            {
+                if (_signaled)
+                {
+                    return;
+                }
+                _signaled = true;
+            }
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            CoreWindow.GetForCurrentThread().Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                lock (this)
+                {
+                    _signaled = false;
+                }
+                Signaled?.Invoke();
+            });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
 
         public IDisposable StartTimer(TimeSpan interval, Action tick)
         {
-            throw new NotImplementedException();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = interval;
+            timer.Tick += (s, e) => tick();
+            timer.Start();
+            return Disposable.Create(timer.Stop);
         }
     }
 }
